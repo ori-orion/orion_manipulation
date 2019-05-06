40c8780804e20adc14ab31b4dc55f5336cc8ac57
Mark <mark.finean@stx.ox.ac.uk>
2019-05-06 20:11:04 +0100

    Give object to operator server refactored and now uses the orion_actions msgs. Eliminated the suction control for simpler testing. Can be added later.

--- manipulation/scripts/give_object_to_operator_server.py
+++ manipulation/scripts/give_object_to_operator_server.py
@@ -1,115 +1,78 @@
 #! /usr/bin/env python
 
 import hsrb_interface
-import hsrb_interface.geometry as geometry
-import numpy as np
 import rospy
 import actionlib
 
 from actionlib_msgs.msg import GoalStatus
-from tmc_suction.msg import (
-    SuctionControlAction,
-    SuctionControlGoal
-)
 
-from manipulation.msg import *
+from orion_actions.msg import GiveObjectToOperatorAction, GiveObjectToOperatorGoal, GiveObjectToOperatorResult
 from orion_hri.msg import WaitForConfirmationAction, WaitForConfirmationGoal, WaitForConfirmationResult
-
-import time 
 
 class GiveObjectToOperatorAction(object):
 
-    
 
     def __init__(self, name):
         self._action_name = 'give_something'
-        self._as = actionlib.SimpleActionServer(self._action_name, manipulation.msg.GiveObjectToOperatorAction,execute_cb=self.execute_cb, auto_start=False)
+        self._as = actionlib.SimpleActionServer(self._action_name, orion_actions.msg.GiveObjectToOperatorAction,execute_cb=self.execute_cb, auto_start=False)
         self._as.start()
 
         # Preparation for using the robot functions
         self.robot = hsrb_interface.Robot()
         self.whole_body = self.robot.try_get('whole_body')
-        self.omni_base = self.robot.try_get('omni_base')
-        self.collision_world = self.robot.try_get('global_collision_world')
         self.gripper = self.robot.try_get('gripper')
 
-        self._HAND_TF = 'hand_palm_link'
-        self._GRASP_FORCE = 0.2
+        # Increase planning timeout. Default is 10s
+        self.whole_body.planning_timeout = 20.0
+        self._CONNECTION_TIMEOUT = 10
 
-	# Define the vacuum timeouts
-	self._CONNECTION_TIMEOUT = 15.0
-	self._SUCTION_TIMEOUT = rospy.Duration(20.0)
+        rospy.loginfo('%s: Initialised. Ready for clients.' % (self._action_name))
 
-	# Increase planning timeout. Default is 10s
-	self.whole_body.planning_timeout = 20.0
-	rospy.loginfo('%s: Initialised. Ready for clients.' % ( self._action_name))
-
-    def execute_cb(self, goal_msg):
-
-	# Create action client to speech confirmation
+    def get_speech_confirmation(self):
+        # Create action client to speech confirmation
         speech_confirm_action = 'wait_for_confirmation'
-        speech_confirm_client = actionlib.SimpleActionClient(
-	    speech_confirm_action, WaitForConfirmationAction)
+        speech_confirm_client = actionlib.SimpleActionClient(speech_confirm_action, WaitForConfirmationAction)
+        rospy.loginfo(' Connecting to speech confirmation server...')
 
-	rospy.loginfo(' Connecting to speech confirmation server...')
-
-	
         try:
-	    if not speech_confirm_client.wait_for_server(
-		    rospy.Duration(self._CONNECTION_TIMEOUT)):
-	        raise Exception(speech_confirm_action + ' does not exist')
+            if not speech_confirm_client.wait_for_server(rospy.Duration(self._CONNECTION_TIMEOUT)):
+                raise Exception(speech_confirm_action + ' does not exist')
 
-	    # Send a goal to start speech
+            # Send a goal to start speech
             rospy.loginfo('Speech server found. Sending goal...')
             speech_goal = WaitForConfirmationGoal()
             speech_goal.question = "Are you holding the object?"
             speech_goal.possible_inputs = [rospy.getparam("bring_me/confirm_operator_presence"),
-					   rospy.getparam("bring_me/negative_confirmation")]
+                                           rospy.getparam("bring_me/negative_confirmation")]
             # Note this timeout is currently
-	    speech_goal.timeout = rospy.getparam("bring_me/wait_for_confirmation_timeout")
-	    
-	    speech_client.send_goal(speech_goal)
+            speech_goal.timeout = rospy.getparam("bring_me/wait_for_confirmation_timeout")
 
-            if (speech_client.send_goal_and_wait(speech_goal, preempt_timeout = rospy.Duration(20)) ==
-	    	GoalStatus.SUCCEEDED):
-	        rospy.loginfo('Speech succeeded.')
+            speech_confirm_client.send_goal(speech_goal)
+
+            if (speech_confirm_client.send_goal_and_wait(speech_goal, preempt_timeout=rospy.Duration(20)) ==
+                    GoalStatus.SUCCEEDED):
+                rospy.loginfo('Speech succeeded.')
             else:
-                rospy.loginfo('Speech failed.')  
-
+                rospy.loginfo('Speech failed.')
 
         except Exception as e:
             rospy.loginfo('%s: Could not connect to speech. Giving operator object anyway...' % (self._action_name))
-	    pass
+            pass
 
-	rospy.loginfo('%s: Opening gripper.' % (self._action_name))
-	self.gripper.command(1.2)
+    def execute_cb(self, goal_msg):
 
-        # Create action client to control suction
-        suction_action = '/hsrb/suction_control'
-        suction_control_client = actionlib.SimpleActionClient(
-	    suction_action, SuctionControlAction)
+        self.get_speech_confirmation(self)
 
-        # Wait for connection
-	#rospy.loginfo('%s: Connecting to suction server...' % (self._action_name))
-        #try:
-	#    if not suction_control_client.wait_for_server(
-	#	    rospy.Duration(self._CONNECTION_TIMEOUT)):
-	#        raise Exception(suction_action + ' does not exist')
-        #except Exception as e:
-	#    rospy.logerr(e)
-	#    sys.exit(1)
+        rospy.loginfo('%s: Moving to neutral position to present object.' % (self._action_name))
+        self.whole_body.move_to_neutral()
 
-	#rospy.loginfo('%s: Turning off the suction...' % (self._action_name))
+        rospy.loginfo('%s: Opening gripper.' % (self._action_name))
+        self.gripper.command(1.2)
 
-	# Send a goal to stop suction
-	#suction_off_goal = SuctionControlGoal()
-	#suction_off_goal.suction_on.data = False
-	#suction_control_client.send_goal_and_wait(suction_off_goal)
+        rospy.loginfo('%s: Opbject given to operator.' % (self._action_name))
 
-	#rospy.loginfo('%s: Succeeded' % self._action_name)
-
-	_result = GiveObjectToOperatorResult()
-	_result.goal_complete = True
+        _result = GiveObjectToOperatorResult()
+        _result.result = True
         self._as.set_succeeded(_result)
 
 
