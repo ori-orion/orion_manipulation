a5d1c4bc26799a5bd47c8536e25c42ce27f9b345
Mark Finean <mark.finean@stx.ox.ac.uk>
2019-05-16 17:41:01 +0100

    Added close drawer action. Needs testing.

---
+++ manipulation/scripts/close_drawer_client.py
@@ -0,0 +1,32 @@
+#! /usr/bin/env python
+
+import rospy
+import actionlib
+
+
+from orion_actions.msg import *
+
+
+def open_door_client():
+    # Creates the SimpleActionClient, passing the type of the action
+    client = actionlib.SimpleActionClient('close_drawer', CloseDrawerAction)
+
+    print("Waiting for server")
+    client.wait_for_server()
+    print("Finished waiting for server")
+
+    goal_msg = CloseDrawerGoal()
+    client.send_goal(goal_msg)
+    client.wait_for_result()
+
+    return client.get_result()
+
+
+if __name__ == '__main__':
+    try:
+        rospy.init_node('close_drawer_client')
+        result = open_door_client()
+        print("Result:" + str(result.result))
+    except rospy.ROSInterruptException:
+        print("Problem.")
+
