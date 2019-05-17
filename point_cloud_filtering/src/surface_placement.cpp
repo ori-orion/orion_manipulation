158aac5202850eb95b2a85007fe2a8dc1a76c459
Mark Finean <mark.finean@stx.ox.ac.uk>
2019-05-17 11:02:23 +0100

    Added put object on surface server.Started code to determine good surface placement

---
+++ point_cloud_filtering/src/surface_placement.cpp
@@ -0,0 +1,39 @@
+//
+// Created by Mark Finean on 2019-05-16.
+//
+
+#include "point_cloud_filtering/surface_utils.h"
+#include "geometry_msgs/PointStamped.h"
+#include <ros/callback_queue.h>
+
+bool get_surface_location(point_cloud_filtering::GetSurfaceGoal::Request  &req,
+                   point_cloud_filtering::GetSurfaceGoal::Response &res){
+
+    ros::NodeHandle nh;
+    tf::TransformBroadcaster br;
+
+    // To publish cropped door and handle
+    ros::Publisher goal_pub =
+            nh.advertise<sensor_msgs::PointCloud2>("valid_surface_cloud", 1, true);
+
+    // Crop for the handle
+    point_cloud_filtering::SurfacePlacement surface_placer(goal_pub);
+
+    ros::Subscriber sub_handle =
+            nh.subscribe("cloud_in", 1, &point_cloud_filtering::SurfacePlacement::Callback,  &surface_placer);
+
+    return true;
+}
+
+int main(int argc, char** argv) {
+    ros::init(argc, argv, "surface_placement");
+    ros::NodeHandle nh;
+
+    ros::ServiceServer service = nh.advertiseService("surface_placer", get_surface_location);
+
+    ROS_INFO("Ready to detect handles.");
+
+    ros::spin();
+
+    return 0;
+}
