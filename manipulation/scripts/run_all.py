0990ce760ad3fb7fd4f78406557f59018e91f1f7
MatthewMunks <matthewmunks@gmail.com>
2023-05-12 14:21:10 +0100

    Creating a python script to run everythin.

---
+++ manipulation/scripts/run_all.py
@@ -0,0 +1,33 @@
+import rospy;
+
+from pick_up_object_server import PickUpObjectAction;
+from put_object_on_surface_server import PutObjectOnSurfaceAction;
+from give_object_to_operator_server import GiveObjectToOperatorAction;
+from receive_object_from_operator_server import ReceiveObjectFromOperatorAction;
+from point_to_object_server import PointToObjectAction;
+from find_placement_server import PlacementFinder;
+
+if __name__ == '__main__':
+    rospy.init_node("manipulation_node_general");
+
+    pick_up_object = PickUpObjectAction(
+        "pick_up_object", use_collision_map=True, use_grasp_synthesis=False
+    )
+
+    put_object_down = PutObjectOnSurfaceAction(
+        "put_object_on_surface", use_collision_map=True,
+    )
+
+    give_object_to_operator = GiveObjectToOperatorAction(
+        "give_object_to_operator", use_collision_map=False
+    )
+
+    recieve_object_from_operator = ReceiveObjectFromOperatorAction(
+        "receive_object_from_operator", use_collision_map=False
+    )
+
+    point_to_object = PointToObjectAction("point_to_object")
+    
+    find_placement_loc = PlacementFinder()
+
+    rospy.spin();
