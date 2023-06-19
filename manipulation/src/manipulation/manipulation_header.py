0e1f38089cc3c8f67ffa4c6e18181d175a81bb21
Ricardo Cannizzaro <ricardo.cannizzaro@gmail.com>
2023-06-19 17:56:27 +0100

    Setting duration back to 0.

--- manipulation/src/manipulation/manipulation_header.py
+++ manipulation/src/manipulation/manipulation_header.py
@@ -311,7 +311,7 @@ class ManipulationAction(object):
 
         return frame_to_hand
 
-    def lookup_transform(self, source, dest, timeout=rospy.Duration(5)):
+    def lookup_transform(self, source, dest, timeout=rospy.Duration(0)):
         """
         Lookup a transfrom and its timestamp.
         Args:
