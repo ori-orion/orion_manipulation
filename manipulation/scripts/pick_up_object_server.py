35183d3dbf09e61cff03112b955bef5acb315341
MatthewMunks <matthewmunks@gmail.com>
2023-06-21 19:40:00 +0100

    Adding a torso rotation for picking stuff up.

--- manipulation/scripts/pick_up_object_server.py
+++ manipulation/scripts/pick_up_object_server.py
@@ -416,10 +416,13 @@ class PickUpObjectAction(ManipulationAction):
             try:
                 self.whole_body.move_end_effector_pose(base_target_pose, self.BASE_FRAME);
             except:
+                BASE_ROTATION = math.pi/2;
                 rospy.logwarn("Initial planning failed.");
                 self.whole_body.move_to_joint_positions({
                     'arm_lift_joint':0.5,
-                    'arm_flex_joint':-0.9*math.pi/2});
+                    # 'arm_flex_joint':-0.9*math.pi/2,
+                    'head_pan_joint':-BASE_ROTATION});
+                self.omni_base.go_rel(0,0,BASE_ROTATION,10);
                 rospy.loginfo("Recomputing");
                 base_target_pose = self.get_relative_effector_pose(
                     goal_tf, relative=chosen_pregrasp_pose, publish_tf="goal_pose", approach_axis=approach_axis
